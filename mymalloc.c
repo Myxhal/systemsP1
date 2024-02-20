b6ba871cd14cf2e9be1d98a64d3a120ea4d08e7a
Sebe9 <sebastian.hanna03@gmail.com>
2024-02-20 18:28:32 -0500

    Finished the logic

--- mymalloc.c
+++ mymalloc.c
@@ -2,31 +2,31 @@
 #include "stdio.h"
 #include "mymalloc.h"
 #define MEMLENGTH 512
-#define TESTVALUE 3.5
 static double memory[MEMLENGTH];
+
 typedef struct ChunkHeader{
     size_t size;
     int allocated;
-    ChunkHeader* nextChunkHeader;
+    struct ChunkHeader* nextChunkHeader;
+    
 }ChunkHeader;
 void initializeMallocArray(){
     //This chunk of code creates the first chunk header IF the test value is not correctly set.
     // I think we are allowed to do this because 
-    if (memory[0] != TESTVALUE){
-        memory[0] = TESTVALUE;
+    if (memory[0] !=0 && memory[1] !=0){
         ChunkHeader firstChunkHeader;
-        firstChunkHeader.size = 509*8; //512 - 1 (For the TESTVALUE) - 2 (For the first Chunk Header)
+        firstChunkHeader.size = 509*8; //512  - 3 (For the first Chunk Header)
         firstChunkHeader.allocated = 0;
         firstChunkHeader.nextChunkHeader = NULL;
-        *((ChunkHeader *)&memory[1]) = firstChunkHeader;
+        *((ChunkHeader *)&memory[0]) = firstChunkHeader;
 }
 
 
     //actually create an array
     //Create the first chunk header
-struct ChunkHeader mArr[MEMLENGTH]; // I don't think we are allowed to do this since the metadata for the storage array has to also be in the storage array
-    struct ChunkHeader header = {0,0};
-    mArr[0] = header;
+//struct ChunkHeader mArr[MEMLENGTH]; // I don't think we are allowed to do this since the metadata for the storage array has to also be in the storage array
+  //  struct ChunkHeader header = {0,0};
+    //mArr[0] = header;
 
 }
 
@@ -37,10 +37,31 @@ size_t round_up(size_t size) {
 
 ChunkHeader* find_free_chunk(size_t size){
     ChunkHeader* current = (ChunkHeader*)((char*) &memory[1]);
+
     while(current != NULL && !(current->allocated && current->size >= size)){
         current = current->nextChunkHeader;
     }
     return current;
+}
+ChunkHeader* correctChunkSize(ChunkHeader* chunkHeader,size_t requestedSize){
+    if(chunkHeader->size > requestedSize){
+        size_t leftoverSize = chunkHeader->size - requestedSize - sizeof(ChunkHeader);
+        chunkHeader->size = requestedSize;
+        //Creates a new chunkheader to head the leftover size and sets its size, pointer, and allocated status accordingly.
+        ChunkHeader newChunkHeader;
+        newChunkHeader.size = leftoverSize;
+        newChunkHeader.nextChunkHeader = chunkHeader->nextChunkHeader;
+        newChunkHeader.allocated = 0;
+        
+        //Puts this chunkheader in the correct place in memory
+        ChunkHeader* addressForNewHeader = chunkHeader + (chunkHeader->size);
+        *addressForNewHeader = newChunkHeader;
+
+        chunkHeader->nextChunkHeader = &addressForNewHeader;
+    }
+    else{
+        return chunkHeader;
+    }
 }
 void * mymalloc(size_t size, char *file, int line){    //I changed the function definition so that it aligns with the way they are defined in mymalloc.h the file and line are used for error messages
     initializeMallocArray();
@@ -50,10 +71,11 @@ void * mymalloc(size_t size, char *file, int line){    //I changed the function 
             return NULL;
     }
 
-    ChunkHeader* chunkHeader = find_free_chunk(size);
+    ChunkHeader* chunkHeader = find_free_chunk(size);//Finds a free chunk that is big enough
     if(chunkHeader == NULL){
         return NULL;
     }
+    chunkHeader = correctChunkSize(chunkHeader,size);//If a chunk is too big this will shrink it to the correct size
     chunkHeader -> allocated = 1;
     return(void*)( chunkHeader +sizeof(ChunkHeader));
     /**
@@ -118,7 +140,7 @@ void coalesce(){
     ChunkHeader *currentChunkHeader = &memory[1];
     ChunkHeader *nextChunkHeader = currentChunkHeader->nextChunkHeader;
     while(nextChunkHeader!=NULL){
-        if ((nextChunkHeader->allocated==0) && (currentChunkHeader->allocated==00)){
+        if ((nextChunkHeader->allocated==0) && (currentChunkHeader->allocated==0)){
             currentChunkHeader->size = currentChunkHeader->size + sizeof(ChunkHeader)+nextChunkHeader->size;
             currentChunkHeader->nextChunkHeader = nextChunkHeader->nextChunkHeader;
             nextChunkHeader = currentChunkHeader->nextChunkHeader;
@@ -132,7 +154,7 @@ void coalesce(){
 }
 int checkIfAligned(ChunkHeader* currentChunkHeader){
     ChunkHeader* iteratedChunkHeader = &memory[1];
-    while(iteratedChunkHeader->next!=NULL){
+    while(iteratedChunkHeader->nextChunkHeader!=NULL){
         if(iteratedChunkHeader+sizeof(ChunkHeader)==currentChunkHeader){
             return 1;
         }
@@ -144,30 +166,35 @@ int checkIfAligned(ChunkHeader* currentChunkHeader){
 }
 void myfree(void *ptr, char *file, int line){
     initializeMallocArray();
+    //If the pointer is null 
     if (!ptr){
         printf("free: null pointer (%s:%d)", file, line);
         return;
-    } //If the pointer is null 
-        
+    } 
+    //If the pointer doesn't point to somewhere in the array. 
     if ((double*)ptr < &memory[1]||(double*)ptr > &memory[499]){
         printf("free: invalid pointer (%s:%d)", file, line);
         return;
-    }//If the pointer doesn't point to somewhere in the array.
+    }
+
     ChunkHeader *currentChunkHeader =  ptr - sizeof(ChunkHeader);//Finds the location of the chunk header of the given chunk
-    bool alignedBool = checkIfAligned(currentChunkHeader);
+    int aligned = checkIfAligned(currentChunkHeader);
+    if(!aligned){
+        printf("free: invalid pointer (%s :%d)",file,line);
+    }
+    //If the chunk isn't allocated
     if (currentChunkHeader->allocated==0){
         printf("free: double free (%s :%d)", file, line);
         return;
-    }//If the chunk isn't allocated
+    }
 
     currentChunkHeader->allocated=0;
     coalesce(currentChunkHeader);
 //I'm not sure how to check whether or not a pointer is in the middle of a chunk without directly accessing the memory which isn't allowed.
-
-
 }
 
 int main(int argc, char **argv)
 {
-    printf("%d", round_up(17));
+    //malloc(-5);
+    printf("%d", sizeof(ChunkHeader));
 }
