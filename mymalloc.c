455bb5e614a3103d0daae32c798a4bc5fae61c67
Myxhal <104581082+Myxhal@users.noreply.github.com>
2024-02-20 23:26:39 -0500

    make file

--- mymalloc.c
+++ mymalloc.c
@@ -121,24 +121,24 @@ void myfree(void *ptr, char *file, int line){
     //If the pointer is null 
     if (!ptr){
         printf(stderr,"free: null pointer (%s:%d)\n", file, line);
-        return NULL;
+        return;
     } 
     //If the pointer doesn't point to somewhere in the array. 
     if ((double*)ptr < &memory[1]||(double*)ptr > &memory[499]){
         printf(stderr, "free: invalid pointer (%s:%d)\n", file, line);
-        return NULL;
+        return;
     }
 
     ChunkHeader *currentChunkHeader =  (char*)ptr - sizeof(ChunkHeader);//Finds the location of the chunk header of the given chunk
     int aligned = checkIfAligned(currentChunkHeader);
     if(!aligned){
         printf(stderr,"free: invalid pointer (%s :%d)\n",file,line);
-        return NULL;
+        return;
     }
     //If the chunk isn't allocated
     if (currentChunkHeader->allocated==0){
         printf(stderr,"free: double free (%s :%d)\n", file, line);
-        return NULL;
+        return;
     }
 
     currentChunkHeader->allocated=0;
